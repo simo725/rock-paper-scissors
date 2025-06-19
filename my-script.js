e70102f19c0bfe8f1f73aab84a49843af0ec5305
simo725 <info@simonecavallero.com>
2025-06-19 16:04:02 +0200

    Function to get Human choice and global var Score

--- my-script.js
+++ my-script.js
@@ -1,3 +1,30 @@
+let humanScore = 0
+let computerScore = 0
+
+
+function getHumanChoice(){
+
+    let humanChoice = prompt("What's your choice? Rock, Paper or Scissor?");
+
+    if (humanChoice.toLowerCase() === "rock") {
+
+        console.log("Rock")
+
+    } else if (humanChoice.toLowerCase() === "paper" ) {
+
+        console.log("Paper")
+
+    } else if (humanChoice.toLowerCase() === "scissors") {
+        
+        console.log("Scissors")
+       
+    }
+
+}
+
+getHumanChoice();
+
+
 function getComputerChoice(){
 
     let choice = Math.floor(Math.random() * 3);
